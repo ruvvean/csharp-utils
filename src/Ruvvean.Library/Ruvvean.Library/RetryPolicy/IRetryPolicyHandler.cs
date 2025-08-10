3209735628ae8d0e9b0c5d049c7ead7bcf33b733
Kacper Domagala <ruvvean@gmail.com>
2025-08-10 16:21:07 +0200

    feat: add retry policy

---
+++ src/Ruvvean.Library/Ruvvean.Library/RetryPolicy/IRetryPolicyHandler.cs
@@ -0,0 +1,75 @@
+﻿namespace Ruvvean.Library.RetryPolicy;
+
+/// <summary>
+/// Defines a handler for executing actions and functions with retry logic.
+/// </summary>
+public interface IRetryPolicyHandler
+{
+    /// <summary>
+    /// The default maximum number of retry attempts.
+    /// </summary>
+    public const int DefaultMaxRetryCount = 3;
+
+    /// <summary>
+    /// The default types of exceptions to handle for retry logic.
+    /// </summary>
+    public static readonly Type[] DefaultExceptionTypes = [typeof(Exception)];
+
+    /// <summary>
+    /// Executes the specified synchronous action with retry logic.
+    /// </summary>
+    /// <param name="action">The action to execute.</param>
+    /// <param name="retryCount">The maximum number of retry attempts. Defaults to <see cref="DefaultMaxRetryCount"/>.</param>
+    /// <param name="exceptionTypes">
+    /// The types of exceptions to handle for retry logic. If <c>null</c>, all exceptions are retried.
+    /// </param>
+    void Excecute(Action action, int retryCount = DefaultMaxRetryCount, Type[]? exceptionTypes = null);
+
+    /// <summary>
+    /// Executes the specified synchronous function with retry logic and returns its result.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result returned by the function.</typeparam>
+    /// <param name="func">The function to execute.</param>
+    /// <param name="retryCount">The maximum number of retry attempts. Defaults to <see cref="DefaultMaxRetryCount"/>.</param>
+    /// <param name="exceptionTypes">
+    /// The types of exceptions to handle for retry logic. If <c>null</c>, all exceptions are retried.
+    /// </param>
+    /// <returns>The result of the function.</returns>
+    TResult Execute<TResult>(Func<TResult> func, int retryCount = DefaultMaxRetryCount, Type[]? exceptionTypes = null);
+
+    /// <summary>
+    /// Executes the specified asynchronous action with retry logic.
+    /// </summary>
+    /// <param name="action">
+    /// The asynchronous action to execute. The action should return a <see cref="Task"/>.
+    /// </param>
+    /// <param name="retryCount">The maximum number of retry attempts. Defaults to <see cref="DefaultMaxRetryCount"/>.</param>
+    /// <param name="exceptionTypes">
+    /// The types of exceptions to handle for retry logic. If <c>null</c>, all exceptions are retried.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// The <see cref="CancellationToken"/> to observe while waiting for the task to complete.
+    /// </param>
+    /// <returns>A <see cref="Task"/> that represents the asynchronous operation.</returns>
+    Task ExecuteAsync(Func<CancellationToken, Task> action, int retryCount = DefaultMaxRetryCount, Type[]? exceptionTypes = null, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Executes the specified asynchronous function with retry logic and returns its result.
+    /// </summary>
+    /// <typeparam name="TResult">The type of the result returned by the function.</typeparam>
+    /// <param name="func">
+    /// The asynchronous function to execute. The function should return a <see cref="Task{TResult}"/>.
+    /// </param>
+    /// <param name="retryCount">The maximum number of retry attempts. Defaults to <see cref="DefaultMaxRetryCount"/>.</param>
+    /// <param name="exceptionTypes">
+    /// The types of exceptions to handle for retry logic. If <c>null</c>, all exceptions are retried.
+    /// </param>
+    /// <param name="cancellationToken">
+    /// The <see cref="CancellationToken"/> to observe while waiting for the task to complete.
+    /// </param>
+    /// <returns>
+    /// A <see cref="Task{TResult}"/> that represents the asynchronous operation, containing the
+    /// result of the function.
+    /// </returns>
+    Task<TResult> ExecuteAsync<TResult>(Func<CancellationToken, Task<TResult>> func, int retryCount = DefaultMaxRetryCount, Type[]? exceptionTypes = null, CancellationToken cancellationToken = default);
+}
---
+++ src/Ruvvean.Library/Ruvvean.Library/RetryPolicy/RetryPolicyHandler.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Logging;
+using Polly;
+using Polly.Retry;
+
+namespace Ruvvean.Library.RetryPolicy;
+
+/// <inheritdoc cref="IRetryPolicyHandler"/>
+public class RetryPolicyHandler(ILoggerFactory loggerFactory) : IRetryPolicyHandler
+{
+    private readonly ILogger<RetryPolicyHandler> logger = loggerFactory.CreateLogger<RetryPolicyHandler>();
+
+    /// <inheritdoc/>
+    public void Excecute(Action action, int retryCount = 3, Type[]? exceptionTypes = null)
+    {
+        var retryPolicy = this.GetRetryPolicy(retryCount, exceptionTypes);
+        retryPolicy.Execute(action);
+    }
+
+    /// <inheritdoc/>
+    public TResult Execute<TResult>(Func<TResult> func, int retryCount = 3, Type[]? exceptionTypes = null)
+    {
+        var retryPolicy = this.GetRetryPolicy(retryCount, exceptionTypes);
+        return retryPolicy.Execute(func);
+    }
+
+    /// <inheritdoc/>
+    public async Task ExecuteAsync(Func<CancellationToken, Task> action, int retryCount = 3, Type[]? exceptionTypes = null, CancellationToken cancellationToken = default)
+    {
+        var retryPolicy = this.GetRetryPolicyAsync(retryCount, exceptionTypes);
+        await retryPolicy.ExecuteAsync(action, cancellationToken);
+    }
+
+    /// <inheritdoc/>
+    public Task<TResult> ExecuteAsync<TResult>(Func<CancellationToken, Task<TResult>> func, int retryCount = 3, Type[]? exceptionTypes = null, CancellationToken cancellationToken = default)
+    {
+        var retryPolicy = this.GetRetryPolicyAsync(retryCount, exceptionTypes);
+        return retryPolicy.ExecuteAsync(func, cancellationToken);
+    }
+
+    private Polly.Retry.RetryPolicy GetRetryPolicy(int retryCount, Type[]? exceptionTypes)
+    {
+        Type[] handled = (exceptionTypes != null && exceptionTypes.Length > 0)
+            ? exceptionTypes
+            : [typeof(Exception)];
+
+        var policyBuilder = Policy.Handle<Exception>(ex => handled[0].IsInstanceOfType(ex));
+
+        for (int i = 1; i < handled.Length; i++)
+        {
+            var t = handled[i];
+            policyBuilder = policyBuilder.Or<Exception>(ex => t.IsInstanceOfType(ex));
+        }
+
+        return policyBuilder.WaitAndRetry(
+            retryCount,
+            retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+            onRetry: (ex, timeSpan, retryCount, context) => logger.LogWarning(ex, "Retry {RetryCount} after {TimeSpan} due to {ExceptionType}", retryCount, timeSpan, ex.GetType().Name));
+    }
+
+    private AsyncRetryPolicy GetRetryPolicyAsync(int retryCount, Type[]? exceptionTypes)
+    {
+        Type[] handled = (exceptionTypes != null && exceptionTypes.Length > 0)
+            ? exceptionTypes
+            : [typeof(Exception)];
+
+        var policyBuilder = Policy.Handle<Exception>(ex => handled[0].IsInstanceOfType(ex));
+
+        for (int i = 1; i < handled.Length; i++)
+        {
+            var t = handled[i];
+            policyBuilder = policyBuilder.Or<Exception>(ex => t.IsInstanceOfType(ex));
+        }
+
+        return policyBuilder.WaitAndRetryAsync(
+            retryCount,
+            retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+            onRetry: (ex, timeSpan, retryCount, context) => logger.LogWarning(ex, "Retry {RetryCount} after {TimeSpan} due to {ExceptionType}", retryCount, timeSpan, ex.GetType().Name));
+    }
+}
